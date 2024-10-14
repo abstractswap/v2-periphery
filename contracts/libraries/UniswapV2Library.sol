94d2f02f6cef3c85695cfb005031f8f491db7293
msvstj <savastijonok@gmail.com>
2024-10-15 00:42:34 +0300

    feat: support zksync

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -18,10 +18,11 @@ library UniswapV2Library {
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
         pair = address(uint(keccak256(abi.encodePacked(
-                hex'ff',
-                factory,
-                keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                bytes32(0x2020dba91b30cc0006188af794c2fb30dd8520db7e2c088b7fc7c103c00ca494), // keccak256("zksyncCreate2")
+                bytes32(uint256(uint160(factory))), // sender
+                keccak256(abi.encodePacked(token0, token1)), // salt
+                hex'0100052734c4d75c5acbdc1a3a934476b46a054330ef6ea080b0f3241ef3f4c1', // init code hash
+                bytes32(0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470) // constructor input hash: keccak256("")
             ))));
     }
 
---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-etherscan'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-typechain'
+import 'hardhat-watcher'
+import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-verify'
+
+export default {
+  networks: {
+    zkSyncTestNode: {
+      url: 'http://localhost:8011',
+      ethNetwork: '',
+      zksync: true,
+    },
+    zkSyncTestnet: {
+      url: 'https://testnet.era.zksync.dev',
+      ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      zksync: true,
+      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
+    },
+    zkSyncMainnet: {
+      url: 'https://mainnet.era.zksync.io',
+      ethNetwork: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      zksync: true,
+      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
+    },
+    abstract: {
+        url: 'https://api.testnet.abs.xyz',
+        ethNetwork: 'sepolia',
+        zksync: true,
+        verifyURL: 'https://explorer.testnet.abs.xyz/contract_verification',
+    },
+  },
+  defaultNetwork: 'zkSyncTestNode',
+  solidity: {
+    version: '0.6.6',
+  },
+  zksolc: {
+    version: '1.3.13',
+    compilerSource: 'binary',
+    settings: {
+      metadata: {
+        bytecodeHash: 'none',
+      },
+    },
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: 'test', params: { testFiles: ['{path}'] } }],
+      files: ['./test/**/*'],
+      verbose: true,
+    },
+  },
+  mocha: {
+    timeout: 100000000,
+  },
+}
